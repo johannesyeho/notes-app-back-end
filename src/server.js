cb276f977ee37219c5642377354ecb5b565836d3
johannesyeho <geriyusuffafat@gmail.com>
2022-07-02 09:02:06 +0700

    fix bugs

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import routes from './routes.js';
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['http://notesapp-v1.dicodingacademy.com'],
